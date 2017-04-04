1949775ad2bbc73b651e0b37ceb6d6b5d3a65fe8
Elena Chubukina <lena@valch.name>
2017-04-04 19:07:54 +0300

    Minor changes

---
+++ src/main/java/com/lenach/totalbooker/controllers/BookingController.java
@@ -0,0 +1,13 @@
+package com.lenach.totalbooker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by o.chubukina on 04/04/2017.
+ */
+@Controller
+@RequestMapping("/bookings")
+public class BookingController {
+
+}
