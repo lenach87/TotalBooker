dbe0a083805a757d7637232f59bd3bc886f4ab63
Elena Chubukina <lena@valch.name>
2017-04-06 09:09:33 +0300

    Proper classes structure

--- src/main/java/com/lenach/totalbooker/service/BookingService.java
+++ src/main/java/com/lenach/totalbooker/service/BookingService.java
@@ -3,5 +3,15 @@ package com.lenach.totalbooker.service;
 /**
  * Created by o.chubukina on 06/04/2017.
  */
+@
 public interface BookingService {
+    Device save (Device device);
+
+    Page<Device> findAll(Pageable pageable);
+
+    Device findOne(Long id);
+
+    Page<Device> findByPattern(String pattern, Pageable pageable);
+
+    void delete(Long id);
 }
